8174d7e70ab90550943e89432ef22e03bc7036a8
Nik <nik@drnik.org>
2018-02-20 21:47:11 +0100

    Show that we can access fields of an object

--- names.py
+++ names.py
@@ -4,7 +4,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+class Name:
+
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+
 @app.route('/')
 def random_name():
-    name = 'Fred Frobisher'
+    name = Name('Fred', 'Frobisher')
     return render_template('name.html', name=name)
