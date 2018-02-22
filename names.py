7ce1619a1722346c10fef5174991b8894487aa89
Nik <nik@drnik.org>
2018-02-22 17:46:51 +0100

    Read CSV from file, select and display random entry
    Note that we also have to decode the strings in the template because they
    contain umlauts. I thought that Jinja handles this per default, but what do I
    know?

--- names.py
+++ names.py
@@ -1,16 +1,55 @@
 #!/usr/bin/env python
 
 from flask import Flask, render_template
+import csv
+import logging
+import random
+import sys
 
-app = Flask(__name__)
+CSV_FILE = 'names.csv'
+CSV_HEADERS = ('Firstname', 'Lastname', 'Firstlastfunny', 'Resolution',
+    'Explanation', 'Author', 'Comment')
+
+logging.basicConfig(level=logging.DEBUG)
 
 class Name:
 
-    def __init__(self, firstname, lastname):
+    def __init__(self, firstname, lastname, firstlastfunny):
         self.firstname = firstname
         self.lastname = lastname
+        self.firstlastfunny = firstlastfunny
+
+def read_csv(f):
+    dr = csv.DictReader(f, fieldnames=CSV_HEADERS, delimiter=';')
+    names = []
+    for row in dr:
+        logging.debug('Got CSV row: %s' % row)
+        name = Name(row['Firstname'], row['Lastname'], row['Firstlastfunny'])
+
+        # The fields are not mandatory, so reading may fail gracefully with a
+        # default value.
+        name.resolution = row.get('Resolution')
+        name.explanation = row.get('Explanation')
+        name.author = row.get('Author')
+        name.comment = row.get('Comment')
+        logging.debug('Read name with firstname %s and lastname %s' %
+                (name.firstname, name.lastname))
+        names.append(name)
+    return names
+
+#
+# Initialization.
+#
+app = Flask(__name__)
+
+#import pdb; pdb.set_trace()
+
+names = read_csv(open(CSV_FILE))
 
 @app.route('/')
 def random_name():
-    name = Name('Fred', 'Frobisher')
+
+    # This ignores the first row, which contains the header.
+    name = random.choice(names[1:])
+
     return render_template('name.html', name=name)
