75300fff6de1a34b2c838c7450f5197d17d094d2
Nik <nik@drnik.org>
2018-02-23 19:02:13 +0100

    Add a first Selenium test
    May throw an error, but nevertheless works. You can show that it works by, for
    example, checking for "Fistname" instead of "Firstname", then the test fails,
    just as it should.

---
+++ seleniumtest.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.PhantomJS()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_login_page(self):
+        self.browser.get('http://127.0.0.1:5000')
+        t = self.browser.find_element_by_tag_name('p').text
+        self.assertIn('Firstname:', t)
+
+if __name__ == '__main__':
+    unittest.main()
