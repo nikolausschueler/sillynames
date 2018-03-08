009de539ea61c273f01a6274345d55bc412deb6b
Nik <nik@drnik.org>
2018-03-08 20:44:13 +0100

    Test button click and display of fun name
    Also, factor out reading name list, as it is used in more than one place.

--- seleniumtest.py
+++ seleniumtest.py
@@ -10,6 +10,7 @@ class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.PhantomJS()
         self.browser.implicitly_wait(3)
+        self.names = names.Name.from_csv(open(names.CSV_FILE))
 
     def tearDown(self):
         self.browser.quit()
@@ -21,10 +22,23 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://127.0.0.1:5000')
         t = self.browser.find_element_by_id('name').text
 
-        allnames = names.Name.from_csv(open(names.CSV_FILE))
         found = False
-        for name in allnames:
+        for name in self.names:
             if name.get_puzzle_name().decode('utf-8') in t:
+                found = True
+        self.assertTrue(found)
+
+    def test_funny_name(self):
+        '''
+        Check that name is displayed.
+        '''
+        self.browser.get('http://127.0.0.1:5000')
+        t = self.browser.find_element_by_xpath('//input[@value="Zeigs mir"]').click()
+        t = self.browser.find_element_by_id('show').text
+
+        found = False
+        for name in self.names:
+            if name.get_funny_name().decode('utf-8') in t:
                 found = True
         self.assertTrue(found)
 
