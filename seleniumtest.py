ff8bf73d49e9cf5fcdb9d05a0d381c7a1a1b2d5b
Nik <nik@drnik.org>
2018-03-08 20:32:59 +0100

    Make Selenium check the name

--- seleniumtest.py
+++ seleniumtest.py
@@ -3,6 +3,8 @@
 from selenium import webdriver
 import unittest
 
+import names
+
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
@@ -14,8 +16,14 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_login_page(self):
         self.browser.get('http://127.0.0.1:5000')
-        t = self.browser.find_element_by_tag_name('p').text
-        self.assertIn('Name:', t)
+        t = self.browser.find_element_by_id('name').text
+
+        allnames = names.Name.from_csv(open(names.CSV_FILE))
+        found = False
+        for name in allnames:
+            if name.get_puzzle_name().decode('utf-8') in t:
+                found = True
+        self.assertTrue(found)
 
 if __name__ == '__main__':
     unittest.main()
