44ab03b0da005fe5c66d24f007e27633fccbcd56
Nik <nik@drnik.org>
2018-03-08 21:03:28 +0100

    Add test for resolution, remove some unneeded vars

--- seleniumtest.py
+++ seleniumtest.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from selenium.common.exceptions import NoSuchElementException
 from selenium import webdriver
 import unittest
 
@@ -33,12 +34,39 @@ class NewVisitorTest(unittest.TestCase):
         Check that name is displayed.
         '''
         self.browser.get('http://127.0.0.1:5000')
-        t = self.browser.find_element_by_xpath('//input[@value="Zeigs mir"]').click()
+        self.browser.find_element_by_xpath('//input[@value="Zeigs mir"]').click()
         t = self.browser.find_element_by_id('show').text
 
         found = False
         for name in self.names:
             if name.get_funny_name().decode('utf-8') in t:
+                found = True
+        self.assertTrue(found)
+
+    def test_resolution(self):
+        '''
+        Check that name is displayed.
+        '''
+        self.browser.get('http://127.0.0.1:5000')
+        self.browser.find_element_by_xpath('//input[@value="Zeigs mir"]').click()
+
+        # Not all funny names need to be explained, so if the button is not
+        # there, we don't test.
+        # The value (displayed text) of this button contains an umlaut, so we
+        # better search it by id, umlauts in an XPath, as we use it for the
+        # first button, won't work.
+        be = None
+        try:
+            be = self.browser.find_element_by_id('explainbutton')
+        except NoSuchElementException:
+            return
+        if be:
+            be.click()
+            t = self.browser.find_element_by_id('explain').text
+
+        found = False
+        for name in self.names:
+            if name.resolution.decode('utf-8') in t:
                 found = True
         self.assertTrue(found)
 
