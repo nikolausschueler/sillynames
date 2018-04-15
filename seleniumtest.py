bc9d2e6b7349c927259388278ba6a5b3884f561c
Nik <nik@drnik.org>
2018-04-15 15:29:54 +0200

    Better comments for tests

--- seleniumtest.py
+++ seleniumtest.py
@@ -32,7 +32,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_funny_name(self):
         '''
-        Check that name is displayed.
+        Check that fun verson of name is displayed.
         '''
         self.browser.get('http://127.0.0.1:5000')
         self.browser.find_element_by_xpath('//input[@value="Zeigs mir"]') \
@@ -47,7 +47,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_resolution(self):
         '''
-        Check that name is displayed.
+        Check that resolution is displayed, if available..
         '''
         self.browser.get('http://127.0.0.1:5000')
         self.browser.find_element_by_xpath('//input[@value="Zeigs mir"]') \
